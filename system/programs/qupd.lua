1046b487332014dd5aa13b7209f6abc26de62f1d
TheRealEli310 <33650824+TheRealEli310@users.noreply.github.com>
2020-11-22 10:44:15 +1100

    Update qupd.lua

--- system/programs/qupd.lua
+++ system/programs/qupd.lua
@@ -1,4 +1,4 @@
-local qf = fs.open("/system/updqueue/qupdg.lua","w")
+local qf = fs.open("/updater/updqueue/qupdg.lua","w")
 qf.write("fs.delete(\"/system\")\nshell.run(\"/updater/github TheRealEli310 cosc /system\")\nfs.delete(\"/updater/updqueue/qupdg.lua\")")
 qf.close()
 shell.run("reboot")
