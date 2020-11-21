3552e095fa54fe4c9dbac50c44464b9a8a08c6cd
TheRealEli310 <33650824+TheRealEli310@users.noreply.github.com>
2020-11-22 10:39:44 +1100

    Add files via upload

--- updater/update.lua
+++ updater/update.lua
@@ -1,7 +1,7 @@
 updatepkg = ({...})[1]
 function screen()
 while true do
-shell.run("/system/uisc \"Updating COSC\" \"Don't turn off your pc\"")
+shell.run("/updater/uisc \"Updating COSC\" \"Don't turn off your pc\"")
 sleep(0.2)
 end
 end
